f9feeba5b26a9bc7801efbea9475f7276e7fcbc1
Unknown <rusditoriq101@gmail.com>
2018-10-27 23:12:18 +0700

    Homepage created
    Create homepage, css n js from bmi project

--- datakaryawan/settings.py
+++ datakaryawan/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'datakaryawan.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,4 +119,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
