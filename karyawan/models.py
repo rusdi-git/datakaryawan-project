7c06f02f78edc6bc59db025a9f819bec135a30a9
Unknown <rusditoriq101@gmail.com>
2018-10-27 11:54:01 +0700

    First App Registered
    Found the problem why appnotregistered, use manage.py shell to run api database

--- karyawan/models.py
+++ karyawan/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class karyawan(models.Model):
+class Karyawan(models.Model):
     noreg = models.CharField(max_length=50, unique=True)
-    name = models.CharField(max_length=150)
+    name = models.CharField(max_length=150, null=True, blank=True)
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,4 +1,4 @@
-"""datakaryawan URL Configuration
+"""mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -16,9 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from karyawan import views
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
 ]
